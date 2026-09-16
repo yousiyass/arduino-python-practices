af8ca3367b3ec16237be23a51d338c95f200c8ff
Emre Çınar <118796966+yousiyass@users.noreply.github.com>
2026-09-16 22:17:47 +0300

    Refactor: Translate variable names to English for standardization

--- pc-hardware-monitor/lcd_system_display/lcd_system_display.ino
+++ pc-hardware-monitor/lcd_system_display/lcd_system_display.ino
@@ -15,13 +15,13 @@ void loop() {
   if (Serial.available() > 0) {
     String incoming_data = Serial.readStringUntil('\n');
 
-    //CPU Discovery
+    //find CPU
     int start1 = incoming_data.indexOf("CPU:%") + 5;
     int finish1 = incoming_data.indexOf("GPU:") - 2;
     String merge1 = incoming_data.substring(start1, finish1);
     int cpu = merge1.toInt();
 
-    //GPU Discovery
+    //find GPU
     int start2 = incoming_data.indexOf("GPU:") + 4;
     int finish2 = incoming_data.indexOf("C") - 1;
     String merge2 = incoming_data.substring(start2, finish2);
